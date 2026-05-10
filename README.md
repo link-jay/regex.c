af6016c874741b79a004aeba510091f31a237440
li huijie <li.huijie.lhj@qq.com>
2026-05-10 16:18:41 +0800

    增加测试，调整结构

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # regex.c
 A simple C lib of regex.  
 
+## Quick Start
+```
+cc regex.c test.c -o test.out && ./test.out  
+```
+
 ## Arch
 ![arch](arch.svg)
--- test.c
+++ test.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "regex.h"
 
 void test(char* pattern, char* src[], size_t counts) {
@@ -13,12 +15,12 @@ void test(char* pattern, char* src[], size_t counts) {
 }
 
 int main() {
-  /* char* dst = parse_src("a(bc|d*)"); */
-  /* printf("%s\n", dst); */
-  /* NFA nfa = compile_nfa(dst); */
-  /* printf(nfa_match(nfa, "addd") ? "true\n" : "false\n"); */
-  /* dfs_free_nfa(nfa.start); */
-  /* free(dst); */
+  char* dst = parse_src("(1|2|3|4|5|6|7|8|9|0)+");
+  printf("%s\n", dst);
+  NFA nfa = compile_nfa(dst);
+  printf(nfa_match(nfa, "123") ? "true\n" : "false\n");
+  dfs_free_nfa(nfa.start);
+  free(dst);
   
   /* test("a(bc|d*)", ((char*[]){"abc", "adddd", "a", "abcdd"}), 4); */
   
@@ -38,10 +40,12 @@ int main() {
   assert(regex_match("abc+", "abccc"));
   assert(!regex_match("abc+", "ab"));
   assert(regex_match("(ab)*c", "c"));
+  assert(regex_match("(a|b)c", "bc"));
   assert(regex_match("a(bc)+", "abcbcbc"));
   assert(regex_match("(ab)+c", "abababc"));
   assert(regex_match("a(bc|d)+", "abcbcbcddd"));
   assert(!regex_match("a(bc|d*)", "abcddd"));
+  assert(regex_match("(1|2|3|4|5|6|7|8|9|0)+", "123"));
   
   puts("Success.");
   return 0;
